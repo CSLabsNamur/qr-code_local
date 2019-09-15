d6fca95a1220968266a3e0f145100c9cf45b77b4
Nadekoooo <33668363+ppoitier@users.noreply.github.com>
2019-09-15 16:24:14 +0200

    Add models
    Add first version of the models:
    - Location
    - Course
    - Activity

--- server/models.py
+++ server/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Activity(models.Model):
+    begin = models.DateTimeField()
+    end = models.DateTimeField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    summary = models.TextField(blank=True)
+    course = models.ForeignKey(Course, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return '(%s to %s) at %s' % (self.begin, self.end, self.course)
