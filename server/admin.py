d2ce72e89b8a1053505f84be30286350369a32f2
Nadekoooo <33668363+ppoitier@users.noreply.github.com>
2019-09-22 21:40:32 +0200

    Register models in admin panel

--- server/admin.py
+++ server/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Location, Course, Activity, Teacher, Teaching
 
-# Register your models here.
+admin.site.register(Location)
+admin.site.register(Activity)
+admin.site.register(Course)
+admin.site.register(Teacher)
+admin.site.register(Teaching)
